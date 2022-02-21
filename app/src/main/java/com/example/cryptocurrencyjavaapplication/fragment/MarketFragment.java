2b995c89580ab8a056b40976e58418388ec3014f
mortezakdevelop <m.vortex3537007@gmail.com>
2022-02-21 04:09:50 -0800

    setup recycyerview for market fragment

--- app/src/main/java/com/example/cryptocurrencyjavaapplication/fragment/MarketFragment.java
+++ app/src/main/java/com/example/cryptocurrencyjavaapplication/fragment/MarketFragment.java
@@ -9,6 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
@@ -21,8 +22,24 @@ import android.view.ViewGroup;
 
 import com.example.cryptocurrencyjavaapplication.MainActivity;
 import com.example.cryptocurrencyjavaapplication.R;
+import com.example.cryptocurrencyjavaapplication.adapter.marketRV_Adapter;
 import com.example.cryptocurrencyjavaapplication.databinding.FragmentMarketBinding;
+import com.example.cryptocurrencyjavaapplication.models.cryptolistmodel.AllMarketModel;
+import com.example.cryptocurrencyjavaapplication.models.cryptolistmodel.DataItem;
+import com.example.cryptocurrencyjavaapplication.room.entity.MarketListEntity;
+import com.example.cryptocurrencyjavaapplication.viewmodel.AppViewModel;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 
 public class MarketFragment extends Fragment {
@@ -30,6 +47,10 @@ public class MarketFragment extends Fragment {
     FragmentMarketBinding binding;
     MainActivity mainActivity;
     CollapsingToolbarLayout collapsingToolbarLayout;
+    AppViewModel appViewModel;
+    CompositeDisposable compositeDisposable;
+    List<DataItem> dataItemList;
+    marketRV_Adapter marketRVAdapter;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -43,9 +64,36 @@ public class MarketFragment extends Fragment {
 
         binding = DataBindingUtil.inflate(getLayoutInflater(),R.layout.fragment_market,container,false);
 
+        compositeDisposable = new CompositeDisposable();
+        appViewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+        getMarketListDataFromDb();
         return binding.getRoot();
     }
 
+    private void getMarketListDataFromDb(){
+        Disposable completable = appViewModel.getAllMarketData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<MarketListEntity>() {
+                    @Override
+                    public void accept(MarketListEntity marketListEntity) throws Throwable {
+                        AllMarketModel allMarketModel = marketListEntity.getAllMarketModel();
+                        dataItemList = allMarketModel.getRootData().getCryptoCurrencyList();
+
+
+                        if (binding.marketRv.getAdapter() == null){
+                            marketRVAdapter = new marketRV_Adapter((ArrayList<DataItem>) dataItemList);
+                            binding.marketRv.setAdapter(marketRVAdapter);
+                        }else {
+                            marketRVAdapter = (marketRV_Adapter) binding.marketRv.getAdapter();
+                            marketRVAdapter.updateData((ArrayList<DataItem>) dataItemList);
+                        }
+
+                    }
+                });
+
+        compositeDisposable.add(completable);
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -71,5 +119,11 @@ public class MarketFragment extends Fragment {
 
             }
         });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
